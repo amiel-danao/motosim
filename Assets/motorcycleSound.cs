308840561f651a8937e046d21edc91178e31090f
Amiel <amielrenaissance4@gmail.com>
2022-05-01 23:34:48 +0800

    aaa

---
+++ Assets/motorcycleSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motorcycleSound : MonoBehaviour
+{	
+	[SerializeField] private AudioClip[] audioClips;
+	[SerializeField] private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void PlaySoundAt(int index){
+		audioSource.PlayOneShot(audioClips[index]);
+	}
+	
+	public void StopSound(){
+		
+	}
+}

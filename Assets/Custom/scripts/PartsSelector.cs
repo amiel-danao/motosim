b2513e9ecb00dab08bb32ab46462f30594b0917c
Amiel Danao <amielrenaissance4@gmail.com>
2022-07-06 19:46:12 +0800

    added outline on parts preview

--- Assets/Custom/scripts/PartsSelector.cs
+++ Assets/Custom/scripts/PartsSelector.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class PartsSelector : MonoBehaviour
 {
     [SerializeField] private CinemachineFreeLook _cinemachineFreeLook;
+    [SerializeField] private Outline[] _outlines; 
+
+    private void Start(){
+        ToggleOutlines(false);
+        _outlines[0].enabled = true;
+    }
 
     public void SetCameraTarget(Transform target){
         _cinemachineFreeLook.m_Follow = target;
         _cinemachineFreeLook.m_LookAt = target;
+    }
+
+    public void ToggleOutlines(bool isOn){
+        foreach(var outline in _outlines){
+            outline.enabled = isOn;
+        }
     }
 }
